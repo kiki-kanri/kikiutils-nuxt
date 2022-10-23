6a4dec39037ea6c2a677db2a34866524adea6b4d
kiki-kanri <a470666@gmail.com>
2022-10-23 18:23:23 +0800

    Fixed auto import error and remove mime-db.

--- nuxt.ts
+++ nuxt.ts
@@ -2,6 +2,7 @@ import { defineNuxtModule } from '@nuxt/kit'
 
 export default defineNuxtModule({
 	setup (options, nuxt) {
+		if (!nuxt.options.imports) nuxt.options.imports = {};
 		if (nuxt.options.imports.dirs == undefined) {
 			nuxt.options.imports.dirs = ['./composables'];
 		}
