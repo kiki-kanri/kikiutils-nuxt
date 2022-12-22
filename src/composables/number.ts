abd1d8da1305d36110fa081471801ed78aafd118
kiki-kanri <a470666@gmail.com>
2022-12-22 18:44:18 +0800

    Add toPercent function

--- src/composables/number.ts
+++ src/composables/number.ts
@@ -13,3 +13,11 @@ export const randomNumber = (min: number, max: number | undefined = undefined): 
 
 	return random(min, max);
 }
+
+/**
+ * 傳入分母與分子，獲得百分比數字(含%)
+ */
+export const toPercent = (molecular: number, denominator: number, withSymbol: boolean = true) => {
+	let result = (molecular / denominator * 100).toFixed(2);
+	return withSymbol ? `${result}%` : result;
+}
