8c376021589443858243c883a0a180c5781a9950
kiki-kanri <a470666@gmail.com>
2023-01-08 10:50:30 +0800

    Fixed random bugs

--- src/composables/number.ts
+++ src/composables/number.ts
@@ -1,15 +1,3 @@
-import { random } from 'lodash-es'
-
-/**
- * 獲取隨機數
- *
- * 若未傳入最大值，則範圍為0至傳入的第一個值
- */
-export const randomNumber = (min: number, max?: number): number => {
-	if (max === undefined) [max, min] = [min, 0];
-	return random(min, max);
-}
-
 /**
  * 傳入分母與分子，獲得百分比數字(含%)
  */
--- src/composables/random.ts
+++ src/composables/random.ts
@@ -1,5 +1,17 @@
+import { random } from 'lodash-es'
+
 const RANDOM_LETTERS = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789';
 const RANDOM_LETTERS_LENGTH = RANDOM_LETTERS.length;
+
+/**
+ * 獲取隨機數
+ *
+ * 若未傳入最大值，則範圍為0至傳入的第一個值
+ */
+export const randomNumber = (min: number, max?: number): number => {
+	if (max === undefined) [max, min] = [min, 0];
+	return random(min, max);
+}
 
 /**
  * 獲取隨機字串
@@ -8,7 +20,7 @@ export const randomStr = (minLength: number = 8, maxLength: number = 8): string 
 	let string = '';
 
 	for (let i = 0; i < randomNumber(minLength, maxLength); i++) {
-		string += RANDOM_LETTERS[Math.floor(Math.random() * RANDOM_LETTERS_LENGTH)];
+		string += RANDOM_LETTERS.charAt(Math.floor(Math.random() * RANDOM_LETTERS_LENGTH));
 	}
 
 	return string;
