c6bba0a34605b736b2f4f00e898fa59435dba8d2
kiki-kanri <a470666@gmail.com>
2023-01-08 10:36:09 +0800

    Change file name, fixed bugs, update packages

--- nuxt.ts
+++ nuxt.ts
@@ -4,9 +4,13 @@ export default defineNuxtModule({
 	setup (options, nuxt) {
 		if (!nuxt.options.imports) nuxt.options.imports = {};
 		if (nuxt.options.imports.dirs == undefined) {
-			nuxt.options.imports.dirs = ['./composables'];
+			nuxt.options.imports.dirs = [
+				'./composables',
+				'composables/*/*.{ts,js,mjs,mts}'
+			];
 		}
 
 		nuxt.options.imports.dirs.push('./node_modules/kikiutils-nuxt/src/composables');
+		nuxt.options.imports.dirs.push('./node_modules/kikiutils-nuxt/src/composables/*/*.{ts,js,mjs,mts}');
 	}
 });
