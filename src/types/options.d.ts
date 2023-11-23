659420fc9521900faffc59e1ce90b59de52e2839
kiki-kanri <a470666@gmail.com>
2023-11-23 19:25:23 +0800

    feat: add ModuleOptions interface

---
+++ src/types/options.d.ts
@@ -0,0 +1,45 @@
+import type { RequiredDeep } from 'type-fest';
+
+export type RequiredModuleOptions = RequiredDeep<ModuleOptions>;
+
+export interface ModuleOptions {
+	enabled?: boolean;
+	enabledModules?: {
+		colorMode?: boolean;
+		googleFonts?: boolean;
+		purgecss?: boolean;
+		robots?: boolean;
+		security?: boolean;
+		unocss?: boolean;
+		vueuse?: boolean;
+	};
+
+	importAllComposablesDirTSFiles?: boolean;
+	nuxtOptions?: {
+		experimental?: {
+			headNext?: boolean;
+			inlineSSRStyles?: boolean;
+		};
+
+		nitro?: {
+			compressPublicAssets?: boolean;
+		};
+
+		typescript?: {
+			tsConfig?: {
+				compilerOptions?: {
+					noImplicitOverride?: boolean;
+					noUncheckedIndexedAccess?: boolean;
+					noUnusedLocals?: boolean;
+					noUnusedParameters?: boolean;
+				};
+			};
+
+			typeCheck?: boolean;
+		};
+	};
+
+	enabledVitePlugins?: {
+		removeConsole?: boolean;
+	};
+}
