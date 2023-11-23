d11933f7a8db046b978f64c3bf2625ec9d7d4c12
kiki-kanri <a470666@gmail.com>
2023-11-23 23:30:43 +0800

    fix: correct erroneous declare module names and remove export from schema

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -1,6 +1,6 @@
 import type { ModuleOptions as PurgecssModuleOptions } from 'nuxt-purgecss';
 
-declare module '@nuxt/schema' {
+declare module 'nuxt/schema' {
 	interface NuxtOptions {
 		purgecss?: PurgecssModuleOptions;
 	}
