0935d2fc4e79aa6d5923c49ad69ebf58abeeb3de
kiki-kanri <a470666@gmail.com>
2024-03-07 22:06:57 +0800

    fix: prevent dragover and drop event listeners from blocking non-file related events

--- src/runtime/plugins/event-handlers/drag-and-drop.client.ts
+++ src/runtime/plugins/event-handlers/drag-and-drop.client.ts
@@ -1,6 +1,15 @@
 import { defineNuxtPlugin } from '#app';
 
+function onDragEvent(this: Window, event: DragEvent) {
+	for (const item of event.dataTransfer?.items || []) {
+		if (item.kind === 'file') {
+			event.preventDefault();
+			break;
+		}
+	}
+}
+
 export default defineNuxtPlugin(() => {
-	window.addEventListener('dragover', (event) => event.preventDefault());
-	window.addEventListener('drop', (event) => event.preventDefault());
+	window.addEventListener('dragover', onDragEvent);
+	window.addEventListener('drop', onDragEvent);
 });
