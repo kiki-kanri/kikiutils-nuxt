190b14b8f97cf27d72574a8bb334d40437978673
kiki-kanri <a470666@gmail.com>
2024-05-08 10:28:37 +0800

    refactor: modify parameters passed to `createError`

--- src/runtime/server/utils/error/index.ts
+++ src/runtime/server/utils/error/index.ts
@@ -2,7 +2,7 @@ import { createError } from 'h3';
 
 export const badRequestError = (message: string = 'bad request') => createH3Error(400, message);
 export const conflictError = (message: string = 'conflict') => createH3Error(409, message);
-export const createH3Error = (statusCode: number = 500, statusMessage: string = 'error') => createError({ status: statusCode, statusCode, statusMessage, statusText: statusMessage });
+export const createH3Error = (statusCode: number = 500, message: string = 'error') => createError({ message, status: statusCode, statusCode, statusText: message });
 export const forbiddenError = (message: string = 'forbidden') => createH3Error(403, message);
 export const internalServerError = (message: string = 'error') => createH3Error(500, message);
 export const notFoundError = (message: string = 'not found') => createH3Error(404, message);
