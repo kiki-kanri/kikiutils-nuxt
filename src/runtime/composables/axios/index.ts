be12a2958fb42bd5fc9c40f297d7b8adc2271283
kiki-kanri <a470666@gmail.com>
2024-07-26 18:06:36 +0800

    chore: update axios export method

--- src/runtime/composables/axios/index.ts
+++ src/runtime/composables/axios/index.ts
@@ -1,2 +1,2 @@
-export { axiosInstance, axiosRequest, axiosDelete, $axiosDelete, axiosGet, $axiosGet, axiosPatch, $axiosPatch, axiosPost, $axiosPost, axiosPut, $axiosPut } from '@kikiutils/node/request';
-export type { AxiosRequestData, AxiosRequestParams } from '@kikiutils/node/request';
+export * from '@kikiutils/node/axios';
+export type { AxiosRequestData, AxiosRequestParams } from '@kikiutils/node/axios';
